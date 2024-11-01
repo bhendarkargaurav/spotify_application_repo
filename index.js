d46cc30778ae0c386487c8c38f0e49d9ade4e074
Gaurav <gaurav.bhe.24@gmail.com>
2024-11-01 23:12:01 +0530

    Added user.js schema in models

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const connect = require('./config/database');
 
 const { PORT } = require('./config/serverConfig');
 
+const User = require('./models/user');
+
 const setupAndStartServer = () => {
     const app = express();
 
@@ -11,6 +13,13 @@ const setupAndStartServer = () => {
         console.log(`Server started at port ${PORT}`);
         await connect();
         console.log('mongoDB connected');
+
+        const user = await User.create({
+            username: 'Gaurav',
+            email: 'gaurav123@456.com',
+            password: '123456',
+        });
+        console.log(user);
     })
 }
 
--- models/user.js
+++ models/user.js
@@ -1,24 +1,25 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const userSchema = new mongoose.Schema({
-//   username: { 
-//     type: String, 
-//     required: true, 
-//     unique: true 
-// },
-//   email: { 
-//     type: String,
-//     required: true, 
-//     unique: true 
-// },
-//   password: { 
-//     type: String, 
-//     required: true 
-// },
-//   playlists: [{ 
-//     type: mongoose.Schema.Types.ObjectId, 
-//     ref: 'Playlist' 
-// }]
-// });
+const userSchema = new mongoose.Schema({
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true 
+},
+  email: { 
+    type: String,
+    required: true, 
+    unique: true 
+},
+  password: { 
+    type: String, 
+    required: true 
+},
+  playlists: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Playlist' 
+}]
+});
 
-// module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+module.exports = User;
