4b0a8c0147f4437f5f6872466243756e7db78e4a
Gaurav <gaurav.bhe.24@gmail.com>
2025-01-06 20:29:49 +0530

    how can we send otp by mail is done here

---
+++ services/email-service.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+class EmailService {
+  async sendOTP(email, otp) {
+    try {
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        // service: 'gmail', // or use SMTP server details
+        auth: {
+          user: 'gauravbhendarkar123@gmail.com',
+          pass: 'lunu khko gebl fbjh', // Use App Passwords if 2FA is enabled   lunu khko gebl fbjh
+        },
+      });
+
+      const mailOptions = {
+        from: 'gauravbhendarkar2024@gamil.com',
+        to: email,
+        subject: 'Hii Dear, Your OTP Code is dont share this one time password with anyone',
+        text: `Your OTP code is: ${otp}`,
+      };
+
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Email sent successfully:', info.response);
+      return info;
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw new Error('Failed to send email');
+    }
+  }
+}
+
+module.exports = EmailService;
