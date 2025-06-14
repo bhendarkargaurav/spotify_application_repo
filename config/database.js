3f62066deab26b2e7401fb4d8e2446050276a1f2
Gaurav <gaurav.bhe.24@gmail.com>
2025-06-14 09:26:56 +0530

    add

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const connect = async () => {
-    await mongoose.connect('mongodb+srv://Gaurav:Gaurav25Mongo@cluster0.gw6zgyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'); // database created if not pressend by same name
+    await mongoose.connect('mongodb+srv://Gaurav:Gaurav25Mongo@cluster0.gw6zgyd.mongodb.net/spotify_DB?retryWrites=true&w=majority&appName=Cluster0'); // database created if not pressend by same name
 }
 
 module.exports = connect;
